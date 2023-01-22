79b1abb25463564af16577bfbc7e4c986ee05df8
Jagreet Das Gupta <jagreetdg@gmail.com>
2023-01-22 14:19:21 +0530

    Redesigned Map Markers

--- components/MapComponentDynamic.tsx
+++ components/MapComponentDynamic.tsx
@@ -2,6 +2,7 @@ import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import React, { Component } from "react";
 import { TextInput, TouchableOpacity, View } from "react-native";
 import { StyleSheet } from "react-native";
+import { Image } from "react-native-elements";
 
 class MapComponentDynamic extends Component {
 	render() {
@@ -20,25 +21,45 @@ class MapComponentDynamic extends Component {
 						coordinate={{ latitude: 37.7882, longitude: -122.4327 }}
 						title={"Izakaya Haruchi"}
 						description={"Pub in Japantown"}
-					/>
+					>
+						<Image
+							source={require("../assets/images/pub.png")}
+							style={{ width: 35, height: 35 }}
+						/>
+					</Marker>
 
 					<Marker
 						coordinate={{ latitude: 37.75824, longitude: -122.4523 }}
 						title={"Konbini Mart"}
 						description={"Store on Clarendon Ave"}
-					/>
+					>
+						<Image
+							source={require("../assets/images/mart.png")}
+							style={{ width: 35, height: 35 }}
+						/>
+					</Marker>
 
 					<Marker
 						coordinate={{ latitude: 37.8, longitude: -122.46 }}
 						title={"Sheridan Park"}
 						description={"Park on Sheridan Ave"}
-					/>
+					>
+						<Image
+							source={require("../assets/images/park.png")}
+							style={{ width: 35, height: 35 }}
+						/>
+					</Marker>
 
 					<Marker
 						coordinate={{ latitude: 37.77, longitude: -122.41 }}
 						title={"Gibraltar Bank"}
 						description={"Bank near Costco Wholesale"}
-					/>
+					>
+						<Image
+							source={require("../assets/images/bank.png")}
+							style={{ width: 35, height: 35 }}
+						/>
+					</Marker>
 				</MapView>
 			</View>
 		);
